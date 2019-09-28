9537933af78cbf5f31650daa0f610fc1c9d37297
Caio Humberto Francisco <caio.in.dust@gmail.com>
2019-09-28 15:52:11 -0300

    Reorganization of test projects.

---
+++ test/UnitTests/Diagnostics.HelthChecks/HealthCheckApplicationBuilderExtensionsTests.cs
@@ -0,0 +1,51 @@
+using FluentAssertions;
+using Microsoft.AspNet.Builder;
+using Microsoft.Owin;
+using System;
+using Xunit;
+
+namespace Diagnostics.HelthChecks
+{
+    public class HealthCheckApplicationBuilderExtensionsTests
+    {     
+        [Fact]
+        public void HealthCheckApplicationBuilderExtensionsWithAppNull()
+        {
+            FluentActions.Invoking(() =>
+                HealthCheckApplicationBuilderExtensions.UseHealthChecks(null, null, new PathString(""))
+            )
+                .Should()
+                .Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void HealthCheckApplicationBuilderExtensionsWithAppAndOptionsNull()
+        {
+            FluentActions.Invoking(() =>
+                HealthCheckApplicationBuilderExtensions.UseHealthChecks(null, null, new PathString(""), options: null)
+            )
+                .Should()
+                .Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void HealthCheckApplicationBuilderExtensionsWithAppNullAndPort()
+        {
+            FluentActions.Invoking(() =>
+                HealthCheckApplicationBuilderExtensions.UseHealthChecks(null, null, new PathString(""), 25)
+            )
+                .Should()
+                .Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void HealthCheckApplicationBuilderExtensionsWithAppAndOptionsNullAndPort()
+        {
+            FluentActions.Invoking(() =>
+                HealthCheckApplicationBuilderExtensions.UseHealthChecks(null, null, new PathString(""), 25, null)
+            )
+                .Should()
+                .Throw<ArgumentNullException>();
+        }
+    }
+}
