dca09f87192c55e8aaa16ba4b964c4ef663c4907
caioindust <caio.in.dust@gmail.com>
2020-01-30 15:29:40 -0300

    Created Consolidation Uri HealthCheck based xabaril

---
+++ src/HealthChecks.ConsolidationUris/ConsolidationUriHealthCheckOptions.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace HealthChecks.ConsolidationUris
+{
+    public interface IConsolidationUriOptions
+    {
+        IConsolidationUriOptions UseGet();
+        IConsolidationUriOptions UsePost();
+        IConsolidationUriOptions UseHttpMethod(HttpMethod methodToUse);
+        IConsolidationUriOptions ExpectHttpCode(int codeToExpect);
+        IConsolidationUriOptions ExpectHttpCodes(int minCodeToExpect, int maxCodeToExpect);
+        IConsolidationUriOptions AddCustomHeader(string name, string value);
+    }
+
+    public class ConsolidationUriOptions : IConsolidationUriOptions
+    {
+        public HttpMethod HttpMethod { get; private set; }
+
+        public (int Min, int Max)? ExpectedHttpCodes { get; private set; }
+
+        public Uri Uri { get; }
+
+        private readonly List<(string Name, string Value)> _headers = new List<(string Name, string Value)>();
+
+        internal IEnumerable<(string Name, string Value)> Headers => _headers;
+
+        public ConsolidationUriOptions(Uri uri)
+        {
+            Uri = uri;
+            ExpectedHttpCodes = null;
+            HttpMethod = null;
+        }
+
+        public IConsolidationUriOptions AddCustomHeader(string name, string value)
+        {
+            _headers.Add((name, value));
+            return this;
+        }
+
+        IConsolidationUriOptions IConsolidationUriOptions.UseGet()
+        {
+            HttpMethod = HttpMethod.Get;
+            return this;
+        }
+
+        IConsolidationUriOptions IConsolidationUriOptions.UsePost()
+        {
+            HttpMethod = HttpMethod.Post;
+            return this;
+        }
+
+        IConsolidationUriOptions IConsolidationUriOptions.ExpectHttpCode(int codeToExpect)
+        {
+            ExpectedHttpCodes = (codeToExpect, codeToExpect);
+            return this;
+        }
+
+        IConsolidationUriOptions IConsolidationUriOptions.ExpectHttpCodes(int minCodeToExpect, int maxCodeToExpect)
+        {
+            ExpectedHttpCodes = (minCodeToExpect, maxCodeToExpect);
+            return this;
+        }
+
+        IConsolidationUriOptions IConsolidationUriOptions.UseHttpMethod(HttpMethod methodToUse)
+        {
+            HttpMethod = methodToUse;
+            return this;
+        }
+    }
+
+    public class ConsolidationUriHealthCheckOptions
+    {
+        private readonly List<ConsolidationUriOptions> _urisOptions = new List<ConsolidationUriOptions>();
+
+        internal IEnumerable<ConsolidationUriOptions> UrisOptions => _urisOptions;
+
+        internal HttpMethod HttpMethod { get; private set; }
+
+        internal (int Min, int Max) ExpectedHttpCodes { get; private set; }        
+
+        public ConsolidationUriHealthCheckOptions()
+        {
+            ExpectedHttpCodes = (200, 299);              // DEFAULT  = HTTP Succesful status codes
+            HttpMethod = HttpMethod.Get;
+        }
+
+        public ConsolidationUriHealthCheckOptions UseGet()
+        {
+            HttpMethod = HttpMethod.Get;
+            return this;
+        }
+
+        public ConsolidationUriHealthCheckOptions UsePost()
+        {
+            HttpMethod = HttpMethod.Post;
+            return this;
+        }
+
+        public ConsolidationUriHealthCheckOptions UseHttpMethod(HttpMethod methodToUse)
+        {
+            HttpMethod = methodToUse;
+            return this;
+        }
+
+        public ConsolidationUriHealthCheckOptions AddUri(Uri uriToAdd, Action<IConsolidationUriOptions> setup = null)
+        {
+            var uri = new ConsolidationUriOptions(uriToAdd);
+            setup?.Invoke(uri);
+
+            _urisOptions.Add(uri);
+
+            return this;
+        }
+
+        public ConsolidationUriHealthCheckOptions ExpectHttpCode(int codeToExpect)
+        {
+            ExpectedHttpCodes = (codeToExpect, codeToExpect);
+            return this;
+        }
+
+        public ConsolidationUriHealthCheckOptions ExpectHttpCodes(int minCodeToExpect, int maxCodeToExpect)
+        {
+            ExpectedHttpCodes = (minCodeToExpect, maxCodeToExpect);
+            return this;
+        }
+
+        internal static ConsolidationUriHealthCheckOptions CreateFromUris(IEnumerable<Uri> uris)
+        {
+            var options = new ConsolidationUriHealthCheckOptions();
+
+            foreach (var item in uris)
+            {
+                options.AddUri(item);
+            }
+
+            return options;
+        }
+    }
+}
---
+++ src/HealthChecks.ConsolidationUris/DependencyInjection/ConsolidationUrisHealthCheckBuilderExtensions.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthChecks.ConsolidationUris.DependencyInjection
+{
+
+    public static class ConsolidationUrisHealthCheckBuilderExtensions
+    {
+        const string NAME = "consolidation-uri-group";
+
+               /// <summary>
+        /// Add a health check for multiple uri's.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+        /// <param name="uris">The collection of uri's to be checked.</param>
+        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'uri-group' will be used for the name.</param>
+        /// <param name="failureStatus">
+        /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
+        /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
+        /// </param>
+        /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
+        public static IHealthChecksBuilder AddConsolidationUrlGroup(this IHealthChecksBuilder builder, IEnumerable<Uri> uris, Func<IEnumerable<HealthCheckResult>, HealthCheckResult> consolidation, string name = default, HealthStatus? failureStatus = default, IEnumerable<string> tags = default)
+        {
+            var options = ConsolidationUriHealthCheckOptions.CreateFromUris(uris);
+
+            return builder.Add(new HealthCheckRegistration(
+                name ?? NAME,
+                sp => new ConsolidationUriHealthCheck(options, consolidation),
+                failureStatus,
+                tags));
+        }
+
+        /// <summary>
+        /// Add a health check for multiple uri's.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+        /// <param name="uris">The collection of uri's to be checked.</param>
+        /// <param name="httpMethod">The http method to be used.</param>
+        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'uri-group' will be used for the name.</param>
+        /// <param name="failureStatus">
+        /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
+        /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
+        /// </param>
+        /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
+        public static IHealthChecksBuilder AddConsolidationUrlGroup(this IHealthChecksBuilder builder, IEnumerable<Uri> uris, Func<IEnumerable<HealthCheckResult>, HealthCheckResult> consolidation, HttpMethod httpMethod, string name = default, HealthStatus? failureStatus = default, IEnumerable<string> tags = default)
+        {
+            var options = ConsolidationUriHealthCheckOptions.CreateFromUris(uris);
+            options.UseHttpMethod(httpMethod);
+
+            return builder.Add(new HealthCheckRegistration(
+                name ?? NAME,
+                sp => new ConsolidationUriHealthCheck(options, consolidation),
+                failureStatus,
+                tags));
+        }
+
+        /// <summary>
+        /// Add a health check for multiple uri's.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+        /// <param name="uriOptions">The action used to configured uri values and specified http methods to be checked.</param>
+        /// <param name="name">The health check name. Optional. If <c>null</c> the type name 'uri-group' will be used for the name.</param>
+        /// <param name="failureStatus">
+        /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
+        /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
+        /// </param>
+        /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
+        public static IHealthChecksBuilder AddConsolidationUrlGroup(this IHealthChecksBuilder builder, Action<ConsolidationUriHealthCheckOptions> uriOptions, Func<IEnumerable<HealthCheckResult>, HealthCheckResult> consolidation, string name = default, HealthStatus? failureStatus = default, IEnumerable<string> tags = default)
+        {
+            var options = new ConsolidationUriHealthCheckOptions();
+            uriOptions?.Invoke(options);
+
+            return builder.Add(new HealthCheckRegistration(
+                name ?? NAME,
+                sp => new ConsolidationUriHealthCheck(options, consolidation),
+                failureStatus,
+                tags));
+        }
+    }
+}
+
